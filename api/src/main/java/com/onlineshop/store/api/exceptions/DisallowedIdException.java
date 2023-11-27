78f997b97dec593c8a2b92e899f0281ab10fa009
Radoslav <radosllav.stanev@gmail.com>
2023-11-27 23:33:21 +0200

    Error handling

--- api/src/main/java/com/onlineshop/store/api/exceptions/DisallowedIdException.java
+++ api/src/main/java/com/onlineshop/store/api/exceptions/DisallowedIdException.java
@@ -2,6 +2,6 @@ package com.onlineshop.store.api.exceptions;
 
 public class DisallowedIdException extends RuntimeException{
     public DisallowedIdException(String id) {
-        super(String.format("Enntity with id '%s' doesn't exist.", id));
+        super(String.format("Entity with id '%s' doesn't exist.", id));
     }
 }
---
+++ rest/src/main/java/com/onlineshop/store/rest/controlleradvice/InvalidInput.java
@@ -0,0 +1,27 @@
+package com.onlineshop.store.rest.controlleradvice;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class InvalidInput {
+
+    private List<String> errors;
+
+    public InvalidInput() {
+        this.errors = new ArrayList<>();
+    }
+
+    public void addMessage(String message) {
+        this.errors.add(message);
+    }
+
+    @Override
+    public String toString() {
+        return "errors:" + errors;
+    }
+}
